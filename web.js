fabf2df69478677dc0e450a38335ed38b489fe9f
Mark Warner <mrwarner@outlook.com>
2013-07-05 15:28:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
-var index = new buffer(readFileSync('./index.html'));
+var index = new buffer(fs.readFileSync('./index.html'));
 
 app.get('/', function(request, response) {
   response.send(index.toString());
